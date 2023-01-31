294b037ee8714aeb960be68f12bdc04760c31daa
profjuanpablo <prof.juanpablo@gmail.com>
2023-01-31 19:11:47 -0300

    mvc store - CSS

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
